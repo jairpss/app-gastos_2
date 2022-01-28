4a7a41232cd60eb54938bdd927cebf8836a77feb
Jair Partida <jairpartida1@gmail.com>
2022-01-27 23:00:31 -0700

    initial structure and react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import WebFont from 'webfontloader'
+import Container from './elements/Container'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './components/Login'
+import ExpensesList from './components/ExpensesList'
+import ExpensesByCategory from './components/ExpensesByCategory'
+import EditExpense from './components/EditExpense'
+import UserRegister from './components/UserRegister'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Fonts
+WebFont.load({
+  google: {
+    families: ['Rubik:400,500,600,700', 'sans-erif']
+  }
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Index = () => {
+  return ( 
+    <BrowserRouter>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/user-registration" element={<UserRegister />} />
+          <Route path="/expenses-category" element={<ExpensesByCategory />} />
+          <Route path="/expenses-list" element={<ExpensesList />} />
+          <Route path="/edit-expense/:id" element={<EditExpense />} />
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+    
+   );
+}
+ 
+
+ReactDOM.render(<Index />, document.getElementById('root'));
